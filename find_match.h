dd71c379ecc502dd327f32abf75c691ff5d129a7
Bengal1 <34989887+Bengal1@users.noreply.github.com>
2022-12-12 02:43:52 +0200

    Update find_match.h

--- find_match.h
+++ find_match.h
@@ -5,7 +5,6 @@
 
 char* handle_i(char* exp);
 
-Lines* is_match_in_line(Input* input, Expression* expression_disassembled, char* line, int line_number, Lines* lines,
-                        int num_of_bytes);
+Lines* IsMatchInLine(Input* input, Expression* expression_disassembled, char* line, int line_number, Lines* lines, int num_of_bytes);
 
 #endif
